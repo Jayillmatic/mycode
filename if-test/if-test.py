e867e8f30f3eda34e70774587389fd7259b999db
Jayillmatic <jesse.hammond@gmail.com>
2018-12-04 14:01:07 -0500

    Testing with If

---
+++ if-test/if-test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+user_input = input('please enter hostname:') #user inputs their host name
+print("You told me your hostname is:" + user_input) #output of users input
+hostname= user_input
+if hostname =='mtg':
+	print('hostname matches expected config')
+elif hostname =='mtg':
+	print('hostname matches expected config')
+elif hostname =='mTg':
+	print('hostname matches expected config')
+elif hostname =='MtG':
+	print('hostname matches expected config')
+elif hostname =='Mtg':
+	print('hostname matches expected config')
